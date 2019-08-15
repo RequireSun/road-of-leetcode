b48b26930a8feaadfa5dbe78af7094642dca0cb9
RequireSun <kelvinlpsun@hotmail.com>
2019-08-15 15:27:04 +0800

    feat: 生成 html 的脚本写完了

--- .scripts/build-site.js
+++ .scripts/build-site.js
@@ -3,17 +3,29 @@
 const fs = require('fs');
 const { promisify } = require('util');
 const path = require('path');
+const ART = require('art-template');
 
 const pReadDir = promisify(fs.readdir);
 const pStat = promisify(fs.stat);
 const pReadFile = promisify(fs.readFile);
-const writeFile = promisify(fs.writeFile);
+const pWriteFile = promisify(fs.writeFile);
 
 const PATH_PLAYGROUND = path.join(__dirname, '../playground/');
 const PATH_PLAYGROUND_README = path.join(PATH_PLAYGROUND, 'README.md');
 
-let _p = pReadDir(PATH_PLAYGROUND);
+let tIndex;
 
+let _p = Promise.resolve();
+
+_p = _p.then(() => pReadFile(path.join(__dirname, 'template/README2Index.art')));
+
+// 编译 html 模板
+_p = _p.then((templateContent) => tIndex = ART.compile(templateContent));
+
+// 读取 playground 文件列表
+_p = _p.then(() => pReadDir(PATH_PLAYGROUND));
+
+// 读取 playground 以下的所有内容状态
 _p = _p.then(files => {
     return Promise.all(files.map(file => {
         const _path = path.join(PATH_PLAYGROUND, file);
@@ -25,16 +37,63 @@ _p = _p.then(files => {
     }));
 });
 
+// 判断每个目录下是否有 README.md 文件
 _p = _p.then(stats => {
     return Promise.all(stats.map(stat => {
         if (stat.path === PATH_PLAYGROUND_README) {
-            return stat.path;
+            return {
+                path: path.dirname(stat.path),
+                README: stat.path,
+            };
         } else if (stat.stat.isDirectory()) {
-            return pStat(path.join(stat.path, 'README.md')).then(() => path.join(stat.path, 'README.md'), () => undefined);
+            return Promise.all([
+                pStat(path.join(stat.path, 'README.md')).then(() => path.join(stat.path, 'README.md'), () => undefined),
+                pStat(path.join(stat.path, 'script.html')).then(() => path.join(stat.path, 'script.html'), () => undefined),
+            ]).then(([README, script]) => ({
+                path: stat.path,
+                README,
+                script,
+            }));
         }
     }));
 });
 
+// 读取源文件
 _p = _p.then(files => {
-    console.log(files.filter(file => file));
+    return files.map((file) => {
+        const { README, script } = file;
+        const _ps = [];
+
+        if (README) {
+            _ps.push(pReadFile(README).then(content => {
+                file.contentREADME = content;
+            }));
+        }
+
+        if (script) {
+            _ps.push(pReadFile(script).then(content => {
+                file.contentScript = content;
+            }));
+        }
+
+        return Promise.all(_ps).then(() => file);
+    });
+});
+
+// 渲染模板
+_p = _p.then(files => {
+    return Promise.all(files.map(file => {
+        file.result = tIndex({
+            title: '',
+            content: file.contentREADME || '',
+            script: file.contentScript || '',
+        });
+
+        return file;
+    }));
+});
+
+// 写结果
+_p = _p.then(files => {
+    return files.map(file => pWriteFile(path.join(file.path, file.result)));
 });

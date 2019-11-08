427ae8c3931396cfc41d28f373e1bfb10eb0bf4f
RequireSun <kelvinlpsun@hotmail.com>
2019-11-08 11:47:42 +0800

    feat: 路由和样式 ok

--- javascript/mvvm-react/src/App.tsx
+++ javascript/mvvm-react/src/App.tsx
@@ -6,26 +6,47 @@ import {
   Link,
 } from 'react-router-dom';
 import logo from './images/logo.svg';
-import './styles/App.css';
+import './styles/App.less';
+import Context from './pages/Context';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <div className="App-header-icon">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="App-header-info">
+              <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+              </p>
+              <a className="App-link" href="https://reactjs.org"
+                 target="_blank" rel="noopener noreferrer">
+                Learn React
+              </a>
+            </div>
+          </div>
+          <ul className="App-header-nav">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/Context">Context</Link>
+            </li>
+          </ul>
+        </header>
+        <div className="App-body">
+          <Switch>
+            <Route exact path="/">
+              <h1>Hello!</h1>
+            </Route>
+            <Route path="/Context">
+              <Context></Context>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 };
 
--- javascript/mvvm-react/src/pages/Context.tsx
+++ javascript/mvvm-react/src/pages/Context.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default () => {
     return (
-        <div></div>
+        <div>Context</div>
     );
 };

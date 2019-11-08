773f10b82b7640fe57099192440b5a102e78d17b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-08 14:04:20 +0000

    Deploy RequireSun/road-of-leetcode to github.com/RequireSun/road-of-leetcode.git:gh-pages

--- javascript/mvvm-react/src/pages/Hooks.tsx
+++ javascript/mvvm-react/src/pages/Hooks.tsx
@@ -1,4 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+const ColorState = (prop: any) => {
+    const [color, setColor] = useState('');
+
+    useEffect(() => {
+        setColor(`#${Math.floor(Math.random() * 255).toString(16)}${Math.floor(Math.random() * 255).toString(16)}${Math.floor(Math.random() * 255).toString(16)}`);
+
+        return () => {
+            console.log('destroyed!', color);
+        };
+    });
+
+    return color;
+};
+
+const Comp1 = (props: { name: string; }) => {
+    const color = ColorState(props);
+
+    return (
+        <div>
+            <h1>My Name is: {props.name}</h1>
+            <p>I'm {}</p>
+        </div>
+    );
+};
 
 export default () => {
     return (
